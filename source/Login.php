d23ee3e96ef6005abc40a8751dac3843137b9e10
ankitjain28may <ankitjain28may77@gmail.com>
2017-01-01 17:13:22 +0530

    Rename files and added

---
+++ source/Login.php
@@ -0,0 +1,93 @@
+<?php
+namespace AnkitJain\RegistrationModule;
+use AnkitJain\RegistrationModule\Session;
+require_once (dirname(__DIR__) . '/config/database.php');
+
+class Login
+{
+
+	private $login;
+	private $password;
+	private $key;
+	private $error;
+	private $connect;
+
+	function __construct()
+	{
+		$this->key = 0;
+		$this->connect = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+		$this->error = array();
+	}
+
+	function authLogin($login, $password)
+	{
+
+		$this->login = trim($login);
+		$this->password = trim($password);
+
+		if(empty($this->login))
+		{
+			$this->key = 1;
+			$this->error = array_merge($this->error, ["login" => " *Enter the login field"]);
+		}
+		elseif (preg_match("/^[@]{1}$/", $this->login))
+		{
+			if(filter_var($this->email, FILTER_VALIDATE_EMAIL) == false)
+			{
+			$this->key = 1;
+			$this->error = array_merge($this->error, ["login" => " *Enter correct Email address"]);
+			}
+		}
+		if(empty($this->password)) {
+			$this->key = 1;
+			$this->error = array_merge($this->error, ["password" => " *Enter the password"]);
+		}
+		else
+		{
+			$pass = md5($this->password);
+		}
+
+		if($this->key == 0)
+		{
+			$query = "SELECT * FROM login WHERE email = '$this->login' or username = '$this->login'";
+			if ($result = $this->connect->query($query))
+			{
+				if ($result->num_rows > 0)
+				{
+					$row = $result->fetch_assoc();
+					$loginID = $row['login_id'];
+					$query = "SELECT id FROM register WHERE id = '$loginID' and password = '$pass'";
+					if($result = $this->connect->query($query))
+					{
+						if ($result->num_rows > 0)
+						{
+							Session::put('start', $loginID);
+							return json_encode([
+								"location" => URL."/account.php"
+							]);
+						}
+						else
+						{
+							$this->error = array_merge($this->error, ["password" => " *Invalid password"]);
+							return json_encode($this->error);
+						}
+					}
+				}
+				else
+				{
+					$this->error = array_merge($this->error, ["login" => " *Invalid username or email"]);
+					return json_encode($this->error);
+				}
+			}
+
+		}
+		else
+		{
+			return json_encode($this->error);
+		}
+		$this->connect->close();
+	}
+}
+
+
+?>
---
+++ source/Register.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace AnkitJain\RegistrationModule;
+use AnkitJain\RegistrationModule\Validate;
+use AnkitJain\RegistrationModule\Session;
+require_once (dirname(__DIR__) . '/config/database.php');
+
+class Register
+{
+	protected $error;
+	protected $name;
+	protected $email;
+	protected $username;
+	protected $password;
+	protected $mob;
+	protected $key;
+	protected $obValidate;
+	protected $connect;
+
+	function __construct()
+	{
+		$this->error = array();
+		$this->key = 0;
+		$this->connect = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+		$this->obValidate = new Validate();
+
+	}
+
+	function authRegister($name, $email, $username, $password, $mob)
+	{
+		$this->name = trim($name);
+		$this->email = trim($email);
+		$this->username = trim($username);
+		$this->password = trim($password);
+		$this->mob = trim($mob);
+		if (empty($this->name)) {
+			$this->key = 1;
+			$this->error = array_merge($this->error, ["name" => " *Enter the name"]);
+		}
+
+		if(empty($this->email)) {
+			$this->key = 1;
+			$this->error = array_merge($this->error, ["email" => " *Enter the email address"]);
+		}
+		elseif(filter_var($this->email, FILTER_VALIDATE_EMAIL) == false) {
+			$this->key = 1;
+			$this->error = array_merge($this->error, ["email" => " *Enter correct Email address"]);
+		}
+		else
+		{
+			if($this->obValidate->validateEmailInDb($this->email))
+			{
+				$this->key = 1;
+				$this->error = array_merge($this->error, ["email" => " *Email is already registered"]);
+			}
+		}
+
+		if(empty($this->username)) {
+			$this->key = 1;
+			$this->error = array_merge($this->error, ["username" => " *Enter the username"]);
+		}
+		else
+		{
+			if($this->obValidate->validateUsernameInDb($this->username))
+			{
+				$this->key = 1;
+				$this->error = array_merge($this->error, ["username" => " *Username is already registered"]);
+			}
+		}
+
+		if(empty($this->password)) {
+			$this->key = 1;
+			$this->error = array_merge($this->error, ["password" => " *Enter the password"]);
+		}
+
+		if(empty($this->mob)) {
+			$this->key = 1;
+			$this->error = array_merge($this->error, ["mob" => " *Enter the Mobile Number"]);
+		}
+		elseif (!preg_match("/^[0-9]{10}$/", $this->mob)) {
+			$this->key = 1;
+			$this->error = array_merge($this->error, ["mob" => " *Enter correct Mobile Number"]);
+		}
+
+		if($this->key == 1)
+		{
+			return json_encode($this->error);
+		}
+		else
+		{
+			$this->key = 0;
+			$pass = md5($this->password);
+			$query = "INSERT INTO register VALUES(null, '$this->email', '$this->username', '$pass')";
+			if(!$this->connect->query($query)) {
+				$this->key = 1;
+				echo "You are not registered || Error in registration2";
+			}
+			else
+			{
+				$query = "SELECT id FROM register WHERE email = '$this->email'";
+				if($result = $this->connect->query($query)) {
+					$row = $result->fetch_assoc();
+					$UserId = $row['id'];
+
+					$query = "INSERT INTO login VALUES('$UserId', '$this->name', '$this->email', '$this->username', '$this->mob')";
+					if(!$this->connect->query($query)) {
+						$this->key = 1;
+						echo "You are not registered || Error in registration1";
+					}
+				}
+			}
+		}
+		if ($this->key == 0) {
+			Session::put('start', $loginID);
+			return json_encode([
+				"location" => URL."/account.php"
+			]);
+		}
+		else
+		{
+			return json_encode($this->error);
+		}
+	}
+}
+?>
